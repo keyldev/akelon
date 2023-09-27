72ecdbeb85fed1eb630abfa1401bec23145f36b2
Vasiliy <wudgexyz@gmail.com>
2023-09-28 01:53:40 +0300

    employyee model; IsWeekend method;

--- src/VacationTask/Program.cs
+++ src/VacationTask/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            
+            List<Employee> employees = new List<Employee>();
+
+
+
         }
+
+        static bool IsWeekend(DateTime date)
+        {
+            return date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday;
+        }
+    }
+    class Employee
+    {
+        public string Name { get; set; }
+        public List<DateTime> VacationDays { get; set; }
+        public int RemainingVacationDays { get; set; } = 28;
     }
 }
